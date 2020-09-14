75ac1a169613f11865f1ba70727dd083f1016e38
Mikko Väänäkoski <mikko.multanen@gmail.com>
2020-09-14 21:55:40 +0300

    Make the sample scene a bit more complex

--- Assets/Scripts/AddOnClick.cs
+++ Assets/Scripts/AddOnClick.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class AddOnClick : MonoBehaviour
 {
     public Transform prefab;
+	[Range(0, 1)]
+	public float interval = 0.1f;
 	private Camera _camera;
 	private float delta = 0f;
 	void Start () {
@@ -14,7 +16,7 @@ public class AddOnClick : MonoBehaviour
 	private void Update() {
 		delta -= Time.deltaTime;
 		if (Input.GetMouseButton(0) && delta < 0f) {
-			delta = 0.01f;
+			delta = interval;
 			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 			Vector3 viewPos = _camera.ScreenToViewportPoint(mousePos);
 			if (viewPos.x > 0 && viewPos.x < 1 && viewPos.y > 0 && viewPos.y < 1) {
